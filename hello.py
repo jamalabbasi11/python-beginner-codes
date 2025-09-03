4919ae6de442935f2ea05d18cb792b9979a4c7dd
jamalabbasi11 <jamalabbasi0110@gmail.com>
2025-09-03 23:37:26 +0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# My first Python program
+# Author: Jamal Shahid Abbasi
+
+def greet():
+    name = "Jamal Sir"
+    print(f"Hello, {name}! 🚀 Welcome to my first Python code on GitHub.")
+
+if __name__ == "__main__":
+    greet()
